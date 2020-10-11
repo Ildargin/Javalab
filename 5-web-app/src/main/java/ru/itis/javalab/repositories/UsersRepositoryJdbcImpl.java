badbea28227af1005643184864a22d86bdab6384
Ildar Gainatullin <ildar.gainatullin@gmail.com>
2020-10-11 20:49:06 +0300

    homework 5 done

--- 5-web-app/src/main/java/ru/itis/javalab/repositories/UsersRepositoryJdbcImpl.java
+++ 5-web-app/src/main/java/ru/itis/javalab/repositories/UsersRepositoryJdbcImpl.java
@@ -3,11 +3,6 @@ package ru.itis.javalab.repositories;
 import ru.itis.javalab.models.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,60 +11,28 @@ public class UsersRepositoryJdbcImpl implements UsersRepository {
     private static final String SQL_SELECT_BY_AGE = "SELECT * FROM USERS WHERE AGE = ?";
     private static final String SQL_SELECT = "SELECT * from USERS";
 
-    private DataSource dataSource;
+    private SimpleJdbcTemplate template;
 
-    public UsersRepositoryJdbcImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private RowMapper<User> userRowMapper = row -> User.builder()
+    private final RowMapper<User> userRowMapper = row -> User.builder()
             .id(row.getLong("id"))
             .firstName(row.getString("first_name"))
             .lastName((row.getString("last_name")))
             .age(row.getInt("age"))
             .build();
 
+    public UsersRepositoryJdbcImpl(DataSource dataSource) {
+        this.template = new SimpleJdbcTemplate(dataSource);
+    }
+
     @Override
     public List<User> findALLByAge(Integer age) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
+        List<User> Users = template.query(SQL_SELECT_BY_AGE, userRowMapper, age);
+        return Users;
+    }
 
-        try {
-            connection = dataSource.getConnection();
-            statement = connection.prepareStatement(SQL_SELECT_BY_AGE);
-            statement.setInt(1, age);
-            resultSet = statement.executeQuery();
-
-            List<User> users = new ArrayList<>();
-
-            while (resultSet.next()) {
-                users.add(userRowMapper.mapRow(resultSet));
-            }
-            return users;
-
-        } catch (SQLException e) {
-            throw new IllegalArgumentException(e);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException ignore) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException ignore) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignore) {
-                }
-            }
-        }
+    @Override
+    public List<User> findAll() {
+        return template.query(SQL_SELECT, userRowMapper);
     }
 
     @Override
@@ -77,50 +40,6 @@ public class UsersRepositoryJdbcImpl implements UsersRepository {
         return Optional.empty();
     }
 
-
-    @Override
-    public List<User> findAll() {
-
-        //return template.query(....)
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            statement = connection.prepareStatement(SQL_SELECT);
-            resultSet = statement.executeQuery();
-
-            List<User> users = new ArrayList<>();
-
-            while (resultSet.next()) {
-                users.add(userRowMapper.mapRow(resultSet));
-            }
-            return users;
-
-        } catch (SQLException e) {
-            throw new IllegalArgumentException(e);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException ignore) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException ignore) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignore) {
-                }
-            }
-        }
-    }
 
     @Override
     public Optional<User> findById() {
@@ -139,11 +58,9 @@ public class UsersRepositoryJdbcImpl implements UsersRepository {
 
     @Override
     public void deleteById(Long id) {
-
     }
 
     @Override
     public void delete(User entity) {
-
     }
 }
